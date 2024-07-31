cfbfc9f76c84b6e7d803b547f72f10ad8a6b60e3
Siva honey <sivahoney1988@gmail.com>
2024-07-31 02:33:05 +0000

    nexus siva

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/Nexus-new/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspace/Nexus-new/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/Nexus-new/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/Nexus-new/nexus-project/src/main.rs
